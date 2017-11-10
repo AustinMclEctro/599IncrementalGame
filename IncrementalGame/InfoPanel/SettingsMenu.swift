d3968aed3b4eb39a33b3fa849778b8691eedd3a2
lukeatmycode <luke.kissick1@ucalgary.ca>
2017-11-10 14:52:48 -0700

    Settings menu is now a UIView

--- IncrementalGame/InfoPanel/SettingsMenu.swift
+++ IncrementalGame/InfoPanel/SettingsMenu.swift
@@ -7,42 +7,40 @@
 //
 
 import Foundation
-import SpriteKit
+import UIKit
 
-class SettingsMenu: SKView {
-    var button1 = SKShapeNode()
-    var button2 = SKShapeNode()
+class SettingsMenu: UIView {
+   
     let width: CGFloat = 200.0
     let height: CGFloat = 50.0
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        //two placeholder menu buttons
-        button1 = SKShapeNode(rect: CGRect(origin: CGPoint(x: frame.width/2 - width/2, y:frame.height - height*2) , size: CGSize(width: width, height: height) ) , cornerRadius: 10)
-        button1.fillColor = UIColor.blue.withAlphaComponent(1)
-        self.scene?.addChild(button1)
-
-        button2 = SKShapeNode(rect: CGRect(origin: CGPoint(x: frame.width/2 - width/2, y:frame.height - height*4), size: CGSize(width: width, height: height )) , cornerRadius: 10)
-        button2.fillColor = UIColor.green.withAlphaComponent(1)
-        self.scene?.addChild(button2)
-
-        presentScene(SKScene(size: frame.size))
-
-        //add labels for the buttons
-        let text1 = SKLabelNode(text: "Menu Item 1")
-        text1.horizontalAlignmentMode = .center
-        button1.addChild(text1)
-        text1.position = CGPoint(x: frame.width/2, y:button1.frame.midY)
-        text1.verticalAlignmentMode = .center
-        text1.fontName = "Helvetica-Bold"
-
-        let text2 = SKLabelNode(text: "Menu Item 2")
-        text2.horizontalAlignmentMode = .center
-        button2.addChild(text2)
-        text2.position = CGPoint(x: frame.width/2, y:button2.frame.midY)
-        text2.verticalAlignmentMode = .center
-        text2.fontName = "Helvetica-Bold"
         
+        //start copy paste below for more buttons
+        let button1 =  UIButton(frame: CGRect(x: frame.width/2 - width/2, y: 0 + height, width: width, height: height) )
+        button1.layer.cornerRadius = 15
+        button1.setTitle("Sound", for: .normal)
+        button1.backgroundColor = UIColor.red
+        button1.addTarget(self, action: #selector(settings1), for: .touchUpInside)
+        self.addSubview(button1)
+        // end copy paste
+
+        let button2 = UIButton(frame: CGRect(x: frame.width/2 - width/2, y:0 + height*3, width: width, height: height ))
+        button2.layer.cornerRadius =  15
+        button2.setTitle("Vibration", for: .normal)
+        button2.backgroundColor = UIColor.red
+        self.addSubview(button2)
+        
+        let button3 = UIButton(frame: CGRect(x: frame.width/2 - width/2, y:0 + height*5, width: width, height: height ))
+        button3.layer.cornerRadius =  15
+        button3.setTitle("Remove Banner Ads", for: .normal)
+        button3.backgroundColor = UIColor.red
+        self.addSubview(button3)
+        
+    }
+    
+    @objc func settings1 (sender: UIButton){
         
     }
     
