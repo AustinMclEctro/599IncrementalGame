7b9df940fe5ba4f948acd2b3b73f06dd73735f1c
f430FERRARI <f430FERRARI@users.noreply.github.com>
2017-12-13 18:55:20 -0700

    Implemented fixture functionality upgrade saves

--- IncrementalGame/PlayArea/Fixture.swift
+++ IncrementalGame/PlayArea/Fixture.swift
@@ -25,7 +25,7 @@ class Fixture: GameObject {
     // MARK: Initializers
     
     
-    init(type: ObjectType, at: CGPoint, inZone: Zone, zoneSize: CGSize, upgradeLevel: Int? = nil) {
+    init(type: ObjectType, at: CGPoint, inZone: Zone, zoneSize: CGSize, upgradeLevel: Int? = nil, strength: Float? = nil, bonusAreaAlpha: CGFloat? = nil) {
         
         self.inZone = inZone
         self.zoneSize = zoneSize
@@ -46,7 +46,13 @@ class Fixture: GameObject {
                 let bonusArea = SKShapeNode(circleOfRadius: sizing)
                 bonusArea.setScale(2)
                 bonusArea.fillColor = .green
-                bonusArea.alpha = 0.05
+                
+                if let bAA = bonusAreaAlpha {
+                    bonusArea.alpha = bAA
+                } else {
+                    bonusArea.alpha = 0.05
+                }
+                
                 self.addChild(bonusArea)
                 self.physicsBody?.categoryBitMask = 8
                 self.physicsBody?.collisionBitMask = 0
@@ -54,7 +60,13 @@ class Fixture: GameObject {
                 let grav = SKFieldNode.radialGravityField()
                 grav.categoryBitMask = 1
                 grav.isEnabled = true
-                grav.strength = 35
+                
+                if let s = strength {
+                    grav.strength = s
+                } else {
+                    grav.strength = 35
+                }
+                
                 grav.isExclusive = false
                 grav.falloff = 0.7
                 self.addChild(grav)
@@ -62,7 +74,13 @@ class Fixture: GameObject {
                 let vort = SKFieldNode.vortexField()
                 vort.categoryBitMask = 1
                 vort.isEnabled = true
-                vort.strength = 10
+                
+                if let s = strength {
+                    vort.strength = s
+                } else {
+                    vort.strength = 10
+                }
+                
                 vort.isExclusive = false
                 self.addChild(vort)
             default:
@@ -72,8 +90,8 @@ class Fixture: GameObject {
         self.physicsBody?.isDynamic = false
         self.physicsBody?.allowsRotation = false
         self.physicsBody?.usesPreciseCollisionDetection = true
-        let rangeX = SKRange(lowerLimit: (dimension*2), upperLimit: (inZone.size.width-(dimension*2)))
-        let rangeY = SKRange(lowerLimit: (dimension*2), upperLimit: (inZone.size.height-(dimension*2)))
+        let rangeX = SKRange(lowerLimit: (dimension*2), upperLimit: (zoneSize.width-(dimension*2)))
+        let rangeY = SKRange(lowerLimit: (dimension*2), upperLimit: (zoneSize.height-(dimension*2)))
         let conX = SKConstraint.positionX(rangeX)
         let conY = SKConstraint.positionY(rangeY)
         self.constraints = [conX,conY]
@@ -138,6 +156,8 @@ class Fixture: GameObject {
         static let objectType = "objectType"
         static let lastPosition = "lastPosition"
         static let upgradeLevel = "upgradeLevel"
+        static let strength = "strength"
+        static let bonusAreaAlpha = "bonusAreaAlpha"
         static let zone = "zone"
         static let zoneSize = "zoneSize"
     }
@@ -148,18 +168,40 @@ class Fixture: GameObject {
         try? (aCoder as! NSKeyedArchiver).encodeEncodable(objectType, forKey: PropertyKey.objectType)
         aCoder.encode(self.position, forKey: PropertyKey.lastPosition)
         aCoder.encode(self.upgradeLevel, forKey: PropertyKey.upgradeLevel)
-        aCoder.encode(self.inZone, forKey: PropertyKey.zone) // TODO
-        aCoder.encode(self.zoneSize, forKey: PropertyKey.zoneSize) // TODO
+        aCoder.encode(self.inZone, forKey: PropertyKey.zone)
+        aCoder.encode(self.zoneSize, forKey: PropertyKey.zoneSize)
+        switch objectType {
+            case .Graviton, .Vortex:
+                if let force = self.children[0] as? SKFieldNode {
+                    aCoder.encode(force.strength, forKey: PropertyKey.strength)
+                }
+            case .Bonus:
+                if let bonusArea = self.children[0] as? SKShapeNode {
+                    aCoder.encode(bonusArea.alpha, forKey: PropertyKey.bonusAreaAlpha)
+                }
+            default:
+                break
+        }
     }
     
     
     required convenience init?(coder aDecoder: NSCoder) {
         let loadedObjectType = (aDecoder as! NSKeyedUnarchiver).decodeDecodable(ObjectType.self, forKey: PropertyKey.objectType)
+        var loadedStrength: Float? = nil
+        var loadedBonusAreaAlpha: CGFloat? = nil
+        switch loadedObjectType! {
+            case .Graviton, .Vortex:
+                loadedStrength = aDecoder.decodeFloat(forKey: PropertyKey.strength)
+            case .Bonus:
+                loadedBonusAreaAlpha = aDecoder.decodeObject(forKey: PropertyKey.bonusAreaAlpha) as? CGFloat
+            default:
+                break
+        }
         let loadedLastPosition = aDecoder.decodeCGPoint(forKey: PropertyKey.lastPosition)
-        let loadedZone = aDecoder.decodeObject(forKey: PropertyKey.zone) as! Zone // TODO
+        let loadedZone = aDecoder.decodeObject(forKey: PropertyKey.zone) as! Zone
         let loadedZoneSize = aDecoder.decodeCGSize(forKey: PropertyKey.zoneSize)
         let loadedUpgradeLevel = aDecoder.decodeInteger(forKey: PropertyKey.upgradeLevel)
         
-        self.init(type: loadedObjectType!, at: loadedLastPosition, inZone: loadedZone, zoneSize: loadedZoneSize, upgradeLevel: loadedUpgradeLevel)
+        self.init(type: loadedObjectType!, at: loadedLastPosition, inZone: loadedZone, zoneSize: loadedZoneSize, upgradeLevel: loadedUpgradeLevel, strength: loadedStrength, bonusAreaAlpha: loadedBonusAreaAlpha)
     }
 }
